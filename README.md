ca7dda3f0ca81cc05ee9a7856e48d8d0bd526606
ducna1 <ducna1@hdbank.com.vn>
2022-06-06 08:39:01 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Inter-Bank-ATM-Network-Smart-Contract
+
+## Background
+
+When a client pays a bill or sends money from one bank to another, it takes 2-5 days to process. This is because money is not sent in real-time, but transactions relative to another financial institution (FI) are summed and processed as a batch at the end of the day. A EFT smart contract solves this problem by sharing a ledger between all concerned FIs--a consortium blockchain. Any FI that is part of the consortium and enters into the smart contract sends the EFT transaction on the shared ledger in real-time and does not require a third party to clear and settle the inter-bank transactions.
+
+## Smart Contract in Solidity
+
+The smart contract may or may not be designed to use Ethereum's cryptocurrency (ETH), or to link Canadian Dollars (CAD) to ETH. We make use of only the shared ledger that records the inter-bank transactions and the ability of FIs to consent in a smart contract to a deposit or withdrawal on behalf of the customer's bank account to the other FI's customer's bank account. Here is an example use case.
+
+### 1. Request transfer
+
+Bank A executes `requestTransfer` with a chosen `correlationId`, `fromAccount` = 12345 (bank account number at Bank A), `address` = <blockchain address of Bank B>, `amount` = 1000 (CAD), and `transactionType` = `Deposit`. The function generates a `transferID` by hashing the combination of the blockchain address of Bank A, the blockchain address of Bank B, bank account 12345 at Bank A, the amount, the timestamp of the blockchain transaction, and the correlationId.
+```
+  bytes32 transferId = sha256(msg.sender, to, fromAccount, toAccount, amount, now, correlationId);
+```
+The values are populated and the transfer's `State` is set to `Initiated`. A `TransferRequested` event is recorded on the blockchain with Bank A's blockchain address, Bank B's blockchain address, the correlation ID, and the transfer ID.
+Hashing is used to simulate a universally-unique identifier (UUID).
+  
+### 2. Get details of transfer request
+
+Bank B can call getters like `getFromAccount(bytes32 transferId) returns (uint)` and `getAmount(bytes32 transferId) returns (uint)` to get details of the transfer request. The smart contract's getters allow only the sender and receiver to access these confidential details by checking that the blockchain address of the entity requesting the details corresponds to the sender or receiver's blockchain address for the transfer request.
+
+### 3. Confirm transfer
+
+Bank B executes `confirmTransfer` with the `transferId`. The function checks that the blockchain address matches the intended receiver, and changes the `Transfer`'s state to `Confirmed`. A `TransferConfirmed` event is posted on the blockchain, notifying the sender.
+
+# Contract is deployed on Rinkeby (Ethereum-Testnet) Network.
+## Contract Address :- 0x65Ac627dD5973d087028be8a585EA3Eb0Fb8763d
+## Contract URL :- https://rinkeby.etherscan.io/address/0x65Ac627dD5973d087028be8a585EA3Eb0Fb8763d
+### For now, we have focused on the settlement of the money. Authentication and Validation will be done in further phases.
+
+We have built DApp as well to interact with the contract.
+
+This involves a number of open source projects to work properly:
+
+* [Remix] - Solidity IDE that's used to write, compile and debug Solidity code.
+* [Javascript] - ightweight, interpreted, object-oriented language with first-class functions, and is best known as the scripting language for Web pages | One Word- Awesome.
+* [Solidity] - A contract-oriented programming language for writing smart contracts.
+* [Truffle] - A Development environment, testing framework, and asset pipeline all rolled into one.
+* [MetaMask] - An extension injects the Ethereum web3 API into every website's javascript context, so that dapps can read from the blockchain.
+
+#### Installation - Frontend
+Install the dependencies and devDependencies and start the server.
+
+```sh
+$ npm install 
+$ cd app/js
+$ npm run dev
+```
+#### Installation - Backend
+You do not need to do anything. Contract is already deployed on Blockchain...
+# banking-smart-contract
---
+++ app/src/index.js
@@ -0,0 +1,196 @@
+import Web3 from "web3";
+import starNotaryArtifact from "../../build/contracts/InterBankNetwork.json";
+
+const App = {
+  web3: null,
+  account: null,
+  meta: null,
+
+  start: async function () {
+    const { web3 } = this;
+
+    try {
+      // get contract instance
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = starNotaryArtifact.networks[networkId];
+      this.meta = new web3.eth.Contract(
+        starNotaryArtifact.abi,
+        deployedNetwork.address,
+      );
+
+      // get accounts
+      const accounts = await web3.eth.getAccounts();
+      console.log(accounts)
+      this.account = accounts[0];
+      console.log(this.account)
+    } catch (error) {
+      console.error("Could not connect to contract or chain.");
+    }
+  },
+
+  setStatus: function (message) {
+    const status = document.getElementById("status");
+    status.innerHTML = message;
+  },
+
+  createStar: async function () {
+    const { addBank } = this.meta.methods;
+    let bank = await addBank().send({
+      from: this.account,
+      value: 10000000000000000000,
+    });
+    const status = document.getElementById("bank");
+    if (bank == 0) { status.innerHTML = "Bank is not registered yet. Please try again." }
+    else { status.innerHTML = "Bank has been registered now." }
+  },
+
+  fundBank: async function () {
+    const { fundBank } = this.meta.methods;
+    const fundamount = document.getElementById("fundamount").value;
+    console.log(fundamount);
+    await fundBank().send({
+      from: this.account,
+      value: fundamount,
+    });
+    const status = document.getElementById("balance");
+    status.innerHTML = 'The Bank has been funded now with ' + fundamount + ' wei';
+  },
+
+  terminateNoticeBank: async function () {
+    const { terminateNoticeBank } = this.meta.methods;
+    await terminateNoticeBank().send({
+      from: this.account,
+    });
+  },
+
+  withdrawBank: async function () {
+    const { withdrawBank } = this.meta.methods;
+    await withdrawBank().send({
+      from: this.account,
+    });
+  },
+  getBalance: async function () {
+    const { getBalance } = this.meta.methods;
+    const status = document.getElementById("balance");
+    status.innerHTML = await getBalance().call({
+      from: this.account,
+    });
+  },
+
+
+  // Implement Task 4 Modify the front end of the DAPP
+  requestTransfer: async function () {
+    const { requestTransfer } = this.meta.methods;
+    const Correlation = document.getElementById("Correlation").value;
+    const Customer = document.getElementById("Customer").value;
+    const Address = document.getElementById("Address").value;
+    const Amount = document.getElementById("Amount").value;
+    const Transaction = document.getElementById("RTransaction").value;
+    console.log(Transaction)
+    let tid = await requestTransfer(Correlation, Customer, Address, Amount, Transaction).send({
+      from: this.account,
+    });
+    const transferidelement = document.getElementById("transferid");
+    this.meta.events.TransferRequested({ fromBlock: 0 }, function (error, event) { console.log(error) })
+      .on('data', (log) => {
+        let { returnValues: { from, to, correlationId, transferId }, blockNumber } = log
+        console.log(`transferId = ${transferId}`)
+        transferidelement.innerHTML = `The transaction has been initiated with transfer id :- ${transferId}`;
+      })
+      .on('changed', (log) => {
+        console.log(`Changed: ${log}`)
+      })
+      .on('error', (log) => {
+        console.log(`error:  ${log}`)
+      })
+    
+    
+  },
+
+  confirmTransfer: async function () {
+    const { confirmTransfer } = this.meta.methods;
+    const Transaction = document.getElementById("Transaction").value;
+    await confirmTransfer(Transaction).send({
+      from: this.account,
+    });
+    const status2 = document.getElementById("status2");
+    this.meta.events.TransferConfirmed({ fromBlock: 0 }, function (error, event){})
+      .on('data', (log) => {
+        let { returnValues: { from, transferId }, blockNumber } = log
+        console.log(`transferId = ${transferId}`)
+        status2.innerHTML = `Transaction ${transferId} is confirmed now `;
+      })
+      .on('changed', (log) => {
+        console.log(`Changed: ${log}`)
+      })
+      .on('error', (log) => {
+        console.log(`error:  ${log}`)
+      })
+  },
+
+  getFrom: async function () {
+    const { getFrom } = this.meta.methods;
+    const Transaction = document.getElementById("Transaction").value;
+    const status = document.getElementById("view");
+    status.innerHTML = await getFrom(Transaction).call({
+      from: this.account,
+    });
+  },
+
+  getTo: async function () {
+    const { getTo } = this.meta.methods;
+    const Transaction = document.getElementById("Transaction").value;
+    const status = document.getElementById("view");
+    status.innerHTML = await getTo(Transaction).call({
+      from: this.account,
+    });
+  },
+
+  getFromAccount: async function () {
+    const { getFromAccount } = this.meta.methods;
+    const Transaction = document.getElementById("Transaction").value;
+    const status = document.getElementById("view");
+    status.innerHTML = await getFromAccount(Transaction).call({
+      from: this.account,
+    });
+  },
+
+  getAmount: async function () {
+    const { getAmount } = this.meta.methods;
+    const Transaction = document.getElementById("Transaction").value;
+    const status = document.getElementById("view");
+    status.innerHTML = await getAmount(Transaction).call({
+      from: this.account,
+    }) + " wei";
+  },
+
+  getState: async function () {
+    const { getState } = this.meta.methods;
+    const Transaction = document.getElementById("Transaction").value;
+    const status = document.getElementById("view");
+    let state = await getState(Transaction).call({
+      from: this.account,
+    });
+    if (state == 0) { status.innerHTML = 'None'; }
+    else if (state = 1) { status.innerHTML = 'Initiated'; }
+    else { status.innerHTML = 'Confirmed'; }
+  }
+
+};
+
+window.App = App;
+
+window.addEventListener("load", async function () {
+  if (window.ethereum) {
+    // use MetaMask's provider
+    App.web3 = new Web3(window.ethereum);
+    await window.ethereum.enable(); // get permission to access accounts
+  } else {
+    console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live",);
+    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+    App.web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/v3/c686ba38f9a3471eb12521c633cfa55e"),);
+  }
+
+  App.start();
+});
+
